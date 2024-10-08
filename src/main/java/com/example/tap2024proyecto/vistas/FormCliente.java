db6f58876aa02ccd9fd416ddb04d8612c373cbdc
Josueyrojas <josueyrojas@gmail.com>
2024-10-07 20:12:31 -0600

    Buscaminas Completo

--- src/main/java/com/example/tap2024proyecto/vistas/FormCliente.java
+++ src/main/java/com/example/tap2024proyecto/vistas/FormCliente.java
@@ -1,5 +1,7 @@
 package com.example.tap2024proyecto.vistas;
 
+import com.example.tap2024proyecto.models.ClienteDAO;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -14,10 +16,12 @@ public class FormCliente extends Stage {
     private Button btnGuardar;
     private VBox vBox;
 
+    private ClienteDAO objCte;
 
     private Scene escena;
 
     public FormCliente() {
+        objCte = new ClienteDAO();
         CrearUI();
 
         this.setTitle("Agregar Cliente :)");
@@ -27,10 +31,20 @@ public class FormCliente extends Stage {
 
     private void CrearUI() {
         txtNomCte = new TextField();
+        txtNomCte.setPromptText("Nombre del Cliente");
         txtEmailCte = new TextField();
+        txtEmailCte.setPromptText("Email del Cliente");
         txtTelCte = new TextField();
+        txtTelCte.setPromptText("Telefono del Cliente");
         btnGuardar = new Button("Guardar");
+        btnGuardar.setOnAction(actionEvent -> GuardarCliente());
         vBox = new VBox(txtNomCte, txtEmailCte, txtTelCte);
+        vBox.setPadding(new Insets(10));
+        vBox.setSpacing(10);
+        escena = new Scene(vBox, 150, 150);
+    }
+
+    private void GuardarCliente() {
     }
 
 }
